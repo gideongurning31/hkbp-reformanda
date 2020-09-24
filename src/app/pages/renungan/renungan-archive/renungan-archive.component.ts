3738b7dc00f68ba164a08f36458e361d29472492
Gideon Gurning <gideon.gurning@gmail.com>
2020-09-24 16:33:24 +0700

    Fixed responsive view on renungan page & added css animate

--- src/app/pages/renungan/renungan-archive/renungan-archive.component.ts
+++ src/app/pages/renungan/renungan-archive/renungan-archive.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { animateFadeIn } from 'src/app/utils/common-animation';
 import { Renungan } from '../renungan.content';
 
 @Component({
   selector: 'app-renungan-archive',
   templateUrl: 'renungan-archive.component.html',
   styleUrls: ['./renungan-archive.component.scss'],
+  animations: [animateFadeIn],
 })
 export class RenunganArchiveComponent implements OnInit {
   constructor() {}
--- src/app/pages/renungan/renungan-detail/renungan-detail.component.ts
+++ src/app/pages/renungan/renungan-detail/renungan-detail.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { animateFadeIn } from 'src/app/utils/common-animation';
 import { Renungan } from '../renungan.content';
 
 @Component({
   selector: 'app-renungan-detail',
   templateUrl: 'renungan-detail.component.html',
   styleUrls: ['./renungan-detail.component.scss'],
+  animations: [animateFadeIn],
 })
 export class RenunganDetailComponent implements OnInit {
   constructor() {}
