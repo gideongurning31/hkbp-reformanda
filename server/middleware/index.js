f1cd1603f576d1539f34383ee16c5713ec865f7f
Gideon Gurning <gideon.gurning@gmail.com>
2020-09-25 16:12:44 +0700

    Removed node.js back-end server

--- server/middleware/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const before = require("./middleware-before");
-const after = require("./middleware-after");
-
-module.exports.before = before;
-module.exports.after = after;
--- src/app/service/server.endpoints.ts
+++ src/app/service/server.endpoints.ts
@@ -1,2 +1,1 @@
 export const api = 'http://localhost:8080';
-export const api2 = 'http://localhost:3000/api';
