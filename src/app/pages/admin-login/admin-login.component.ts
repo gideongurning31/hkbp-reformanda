c745ab1b4eb85a0950117a887df94ec42b671f16
Gideon Gurning <gideon.gurning@gmail.com>
2020-09-28 23:14:17 +0700

    Created submit action with validation on login

--- src/app/pages/admin-login/admin-login.component.ts
+++ src/app/pages/admin-login/admin-login.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
 import { animateFadeIn } from 'src/app/utils/common-animation';
 import { BaseFormComponent } from 'src/app/utils/components/base-form.component';
 import { AuthenticationService } from 'src/app/service/authentication.service';
 import { SpinnerCloakService } from 'src/app/utils/components/spinner-cloak/spinner-cloak.service';
-import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-admin-login',
@@ -30,6 +31,28 @@ export class AdminLoginComponent extends BaseFormComponent implements OnInit {
       user: new FormControl(null, Validators.required),
       pass: new FormControl(null, Validators.required),
     });
+    this.formErrors = { user: null, pass: null };
   }
 
+  submit() {
+    if (this.validateForm()) {
+      this.setSpinner(true);
+      const subscription: Subscription = this.authService.login(this.loginForm.value)
+        .subscribe((response: string) => this.onLoginSuccess(subscription, response), (error) => this.onErrorResponse(subscription, error));
+    }
+  }
+
+  private validateForm(): boolean {
+    const values = this.loginForm.value;
+    Object.keys(this.formErrors).forEach((key) => {
+      if (values[key] === null || values[key].trim() === '') this.formErrors[key] = '*kolom wajib diisi!';
+    });
+    return this.noValidationErrors();
+  }
+
+  private onLoginSuccess(subscription: Subscription, response: any) {
+    subscription.unsubscribe();
+    console.info('Login response:', response.token);
+    this.setSpinner(false);
+  }
 }
