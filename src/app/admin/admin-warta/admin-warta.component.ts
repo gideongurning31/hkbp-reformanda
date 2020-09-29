413d9697a919aca98608aed68db4ba90326d91da
Gideon Gurning <gideon.gurning@gmail.com>
2020-09-29 18:22:29 +0700

    Initial form layout for upload warta

--- src/app/admin/admin-warta/admin-warta.component.ts
+++ src/app/admin/admin-warta/admin-warta.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-warta',
-  template: '<div class="coming-soon"><h1>COMING SOON</h1></div>',
-  styles: ['.coming-soon { width: 100%; height: 500px; display: flex; align-items: center; justify-content: center; text-align: center; }']
+  templateUrl: 'admin-warta.component.html',
+  styleUrls: ['../admin-forms.component.scss', 'admin-warta.component.scss'],
 })
 export class AdminWartaComponent implements OnInit {
   constructor() {}
